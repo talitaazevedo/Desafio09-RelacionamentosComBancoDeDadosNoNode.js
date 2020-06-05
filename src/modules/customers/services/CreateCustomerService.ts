904b8b89d15dd0b4ca32304d3ac861e0f3914d44
Talita Azevedo <talita.azevedo360@gmail.com>
2020-06-04 21:12:50 -0300

    :turtle: Create Customers

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,25 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkCustomerExists = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (checkCustomerExists) {
+      throw new AppError('This Customer Mail already exists');
+    }
+
+    const customer = this.customersRepository.create({
+      name,
+      email,
+    });
+    return customer;
   }
 }
 
